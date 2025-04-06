0587fe9d1a664c406b81fc81a065b7f50cc6c66d
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-04-06 14:11:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 int countPairs1(int *arr, int len, int value) {
   return 0;
 }
-int countPairs2(int *arr, int len, int value) {
+int countPairs2(int *arr, iфывфывфывnt len, int value) {
   return 0;
 }
 int countPairs3(int *arr, int len, int value) {
